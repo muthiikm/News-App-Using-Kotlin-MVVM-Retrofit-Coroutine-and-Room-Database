4add95851db466c8c51291adabe303bd427d25ee
muthiikm <muthiikm@gmail.com>
2024-07-09 09:59:30 +0300

    initial commit

---
+++ app/src/androidTest/java/com/news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package com.news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals(
+            "com.news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database",
+            appContext.packageName
+        )
+    }
+}
