9d26bd4d9f220b125c78a3507ffa775f1282bc90
muthiikm <muthiikm@gmail.com>
2024-07-15 22:50:00 +0300

    constants

---
+++ app/src/main/java/com/news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database.util
+
+class Constants {
+    companion object{
+        const val API_KEY="2e7dce6c7e9e452e9ae68c9a095ec9c5"
+        const val BASE_URL ="https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY="500L"
+        const val QUERY_PAGE_SIZE="20"
+
+    }
+}
