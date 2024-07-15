506b526b663cd074ae7beb25280958fe8c37a43e
muthiikm <muthiikm@gmail.com>
2024-07-15 16:36:50 +0300

    ui reaarangement

---
+++ app/src/main/java/com/news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database/ui/NewsActivity.kt
@@ -0,0 +1,21 @@
+package com.news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.news_app_using_kotlin_mvvm_retrofit_coroutine_and_room_database.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_news)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
